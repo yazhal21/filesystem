148f0865e74dc18668c146861299a439906cfc80
yazhal21 <89178646578@mail.ru>
2025-08-25 18:45:15 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -7,43 +7,35 @@
 #include <stddef.h>
 
 #define MAX_FILES 100
-#define MAX_PATH_LENGTH 100
+#define MAX_FILENAME_LENGTH 50
 #define MAX_CONTENT_LENGTH 1000
 
+// Структура файла
 typedef struct {
-    char path[MAX_PATH_LENGTH];
+    char name[MAX_FILENAME_LENGTH];
     char content[MAX_CONTENT_LENGTH];
     size_t size;
-} FileEntry;
+} File;
 
+// Структура файловой системы
 typedef struct {
-    FileEntry entries[MAX_FILES];
-    size_t count;
+    File files[MAX_FILES];
+    int file_count;
 } FileSystem;
 
-/* Базовые операции файловой системы */
+/* Функции работы с файловой системой */
+FILE* open_filesystem(const char* filename);
+int close_filesystem(FILE* fs_file);
+
+/* Функции работы с файлами внутри системы */
+char* view_file(const char* target_filename);
+int delete_file(const char* target_filename);
+int create_new_file(FileSystem* fs, const char* filename, const char* content);
+int modify_file(FileSystem* fs, const char* filename, const char* new_content);
+
+/* Вспомогательные функции */
 int initialize_filesystem(FileSystem* fs);
-int create_new_file(FileSystem* fs, const char* path, const char* content);
-int read_file_content(FileSystem* fs, const char* path, char* buffer);
-int modify_file(FileSystem* fs, const char* path, const char* new_content);
-int delete_file(FileSystem* fs, const char* path);
+int read_file_content(FileSystem* fs, const char* filename, char* buffer);
 void list_files(FileSystem* fs);
-
-/* Загрузка/сохранение/освобождение */
-int  fs_load(const char* fname, FileSystem* fs);
-int  fs_save(const char* fname, const FileSystem* fs);
-void fs_free(FileSystem* fs);
-
-/* CRUD */
-int  fs_select(const FileSystem* fs, const char* path, char** out_content);
-int  fs_insert(FileSystem* fs, const char* path);
-int  fs_update(FileSystem* fs, const char* path, const char* new_content);
-int  fs_delete(FileSystem* fs, const char* path);
-
-/* Новые функции */
-int    fs_crypto(FileSystem* fs, const char* path, const char* key);
-int    fs_decrypto(FileSystem* fs, const char* path, const char* key);
-size_t fs_count(const FileSystem* fs);
-int    fs_rename(FileSystem* fs, const char* old_path, const char* new_path);
 
 #endif /* FILESYSTEM_H */
